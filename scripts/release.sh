a260c786f2da71443ac0867b0dc243e42a18cb14
Bryan Chen <xlchen1291@gmail.com>
2025-03-27 22:03:31 +1300

    chore: enhance release script with branch check and error handling

--- scripts/release.sh
+++ scripts/release.sh
@@ -9,13 +9,20 @@ fi
 
 VERSION=$1
 
+git pull
+
 # require no unstaged changes
 if [ -n "$(git status --porcelain)" ]; then
   echo "Error: Unstaged changes detected. Commit or stash them before running this script."
   exit 1
 fi
 
-git pull
+# require on master branch
+CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+if [ "$CURRENT_BRANCH" != "master" ]; then
+  echo "Error: Must be on master branch to release."
+  exit 1
+fi
 
 # Find package.json files under packages directory and update version
 find ./packages -name package.json -not -path "*/node_modules/*" | while read -r file; do
@@ -28,14 +35,14 @@ echo "Version bumped to $VERSION in all package.json files under packages direct
 git add .
 git commit -m "chore: release $VERSION"
 
-git tag v$VERSION
 bun run clean
 bun run build
-bun publish --cwd packages/core --access public
-bun publish --cwd packages/github --access public
-bun publish --cwd packages/cli-shared --access public
-bun publish --cwd packages/cli --access public
-bun publish --cwd packages/runner --access public
+bun publish --cwd packages/core --access public || exit 1
+bun publish --cwd packages/github --access public || exit 1
+bun publish --cwd packages/cli-shared --access public || exit 1
+bun publish --cwd packages/cli --access public || exit 1
+bun publish --cwd packages/runner --access public || exit 1
+git tag v$VERSION
 git push origin v$VERSION
 git push
 gh release create v$VERSION --generate-notes
