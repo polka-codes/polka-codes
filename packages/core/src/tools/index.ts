66e41cfdf0fe1d4a78d2653f4e2e21ad8eb1f8fb
Bryan Chen <xlchen1291@gmail.com>
2025-06-07 13:17:23 +0800

    refactor: restore replaceInFile export and comment out editFile in allTools

--- packages/core/src/tools/index.ts
+++ packages/core/src/tools/index.ts
@@ -1,5 +1,5 @@
 export * from './provider'
 export * from './allTools'
-export { default as replaceInFile } from './replaceInFile' // not included in allTools
+export { default as editFile } from './editFile' // not included in allTools
 
 export * as allTools from './allTools'
