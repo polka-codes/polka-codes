59309bbbc31b655c8dd94fefdb37051db616ad17
Bryan Chen <xlchen1291@gmail.com>
2025-08-11 10:30:19 +1200

    update model prices

--- packages/cli/src/prices.ts
+++ packages/cli/src/prices.ts
@@ -11,6 +11,7 @@ export type ModelInfo = {
 export default {
   [AiProvider.Anthropic]: {
     'claude-opus-4-20250514': { inputPrice: 15, outputPrice: 75, cacheWritesPrice: 18.75, cacheReadsPrice: 1.5, supportsThinking: true },
+    'claude-opus-4-1-20250805': { inputPrice: 15, outputPrice: 75, cacheWritesPrice: 18.75, cacheReadsPrice: 1.5, supportsThinking: true },
     'claude-sonnet-4-20250514': { inputPrice: 3, outputPrice: 15, cacheWritesPrice: 3.75, cacheReadsPrice: 0.3, supportsThinking: true },
     'claude-3-7-sonnet-20250219': { inputPrice: 3, outputPrice: 15, cacheWritesPrice: 3.75, cacheReadsPrice: 0.3, supportsThinking: true },
     'claude-3-5-sonnet-20241022': { inputPrice: 3, outputPrice: 15, cacheWritesPrice: 3.75, cacheReadsPrice: 0.3, supportsThinking: false },
@@ -29,10 +30,9 @@ export default {
   },
 
   [AiProvider.OpenAI]: {
-    'gpt-4.1': { inputPrice: 2, outputPrice: 8, cacheWritesPrice: 0.5, cacheReadsPrice: 0.5 },
-    'gpt-4.1-mini': { inputPrice: 0.4, outputPrice: 1.6, cacheWritesPrice: 0.1, cacheReadsPrice: 0.1 },
-    'gpt-4.1-nano': { inputPrice: 0.1, outputPrice: 0.4, cacheWritesPrice: 0.025, cacheReadsPrice: 0.025 },
-    'o4-mini': { inputPrice: 1.1, outputPrice: 4.4, cacheWritesPrice: 0.275, cacheReadsPrice: 0.275 },
+    'gpt-5-2025-08-07': { inputPrice: 1.25, outputPrice: 10, cacheWritesPrice: 0, cacheReadsPrice: 0.125, supportsThinking: false },
+    'gpt-5-mini-2025-08-07': { inputPrice: 0.25, outputPrice: 2, cacheWritesPrice: 0, cacheReadsPrice: 0.025, supportsThinking: false },
+    'gpt-5-nano-2025-08-07': { inputPrice: 0.05, outputPrice: 0.4, cacheWritesPrice: 0, cacheReadsPrice: 0.005, supportsThinking: false },
   },
 
   [AiProvider.GoogleVertex]: {
