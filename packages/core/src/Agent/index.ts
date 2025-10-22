b3aeb8f7c7eca0f24b9c1879b173a083989deefd
Bryan Chen <xlchen1291@gmail.com>
2025-10-23 11:01:06 +1300

    remove unneeded code

--- packages/core/src/Agent/index.ts
+++ packages/core/src/Agent/index.ts
@@ -1,18 +1,4 @@
-import { analyzerAgentInfo } from './AnalyzerAgent'
-import { architectAgentInfo } from './ArchitectAgent'
-import { codeFixerAgentInfo } from './CodeFixerAgent'
-import { coderAgentInfo } from './CoderAgent'
-
 export * from './AgentBase'
 export * from './AgentPolicy'
-export * from './AnalyzerAgent'
-export * from './ArchitectAgent'
-export * from './CodeFixerAgent'
-export * from './CoderAgent'
-export * from './MultiAgent'
-export * from './parseAssistantMessage'
 export * from './parseJsonFromMarkdown'
 export * from './prompts'
-
-export const allAgents = [architectAgentInfo, coderAgentInfo, analyzerAgentInfo, codeFixerAgentInfo] as const
-export type AgentNameType = (typeof allAgents)[number]['name']
